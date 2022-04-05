f95b276b2561ff9b263fe2a9cd28e252e2a67711
Joseph <josephgabrieltina@gmail.com>
2022-04-05 16:25:00 +0800

    removed some defaults

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
-// import App from './App';
+
 import Container from './container/container'
+
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App /> */}
     <Container />
   </React.StrictMode>,
   document.getElementById('root')
